8e88cf48da0d7d972338dd34ddeb4d0a2b5f093e
Daniel Enriquez <daniel.enm17@gmail.com>
2022-09-01 13:57:09 -0500

    doctor validations added

--- lecture/active-record-boilerplate/app/models/doctor.rb
+++ lecture/active-record-boilerplate/app/models/doctor.rb
@@ -4,4 +4,7 @@ class Doctor < ActiveRecord::Base
   has_many :patients, through: :consultations
 
   validates :last_name, presence: true
+  validates :first_name, uniqueness: { scope: :last_name }
+  validates :last_name, length: { minimum: 3 }
+  validates :email, format: { with: /\A.*@.*\.com\z/ }
 end
